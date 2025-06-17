e5fb46bd8af158b627abdce90db9b51ec0a1ea4c
MariaShalaginova <pro_m_@mail.ru>
2025-06-18 00:10:18 +0300

    feat: импорт  компонента ProductCard в App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import "./App.css";
+import "./index.css";
+import ProductCard from "./components/ProductCard/ProductCard";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <ProductCard
+        title="Cherry tiggo 7"
+        origin="Китай"
+        price={349000000}
+        currency="RUB"
+        imageUrl="https://acroadtrip.blob.core.windows.net/catalogo-imagenes/s/RT_V_5253c41afead4aa4b4e90bd3c3db68b3.webp"
+      />
+    </>
+  );
 }
 
 export default App;
